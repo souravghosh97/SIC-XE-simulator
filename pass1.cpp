e87591544ef87232b8336cd0e5066e12036c25f5
souravghosh97 <souravghosh2197@gmail.com>
2017-11-12 17:41:45 +0530

    pass2 basic structure

--- pass1.cpp
+++ pass1.cpp
@@ -3,89 +3,226 @@
 #include <fstream>
 #include <sstream>
 #include <unordered_map>
+#include <iomanip>
 #include <vector>
 
 #define MEMORY_SIZE 32168
-
+#define print(i) "^" << setfill('0') << setw(i)
 using namespace std;
 
 typedef char int8;
 typedef short int16;
 typedef pair<int8,int16> int24;
+typedef struct{
+	int opcode;//can be 8 or 6bit
+	int flags;// 6 bit
+	int disp;// 12 or 20 bits
+	int format;
+}instruction;
+
+typedef struct{
+	int8 opcode;
+	int format;
+}object;
+
+typedef struct{
+	int PC;
+	int A;
+	int B;
+	int S;
+	int T;
+	int L;
+	int X;
+}registers;
+
+bool pc_relative = true;//to denote base relative or pc
 
 unordered_map <string, int16> symtab;
-unordered_map <string, int8> optab;
+unordered_map <string, object> optab;
 unordered_map <int8, int8> registertab;
+
+instruction calculate_objectcode(vector<string> vec, int lineno, registers r){
+	instruction result;
+	string str;
+	int i;
+	result.opcode = 0;
+	result.flags = 0;
+	result.disp = 0;
+	result.format = 0;
+	if(vec.size() <= 2)
+		i = 0;
+	else
+		i = 1;
+	/* opcode*/
+	if(vec[i][0] == '+'){
+				str = vec[i].substr(1);
+				if(optab.find(str) != optab.end() && optab[str].format == 3){
+					result.format = optab[str].format + 1;
+					result.opcode = optab[str].opcode & 0xFC;
+					result.flags = result.flags | 0x1;//0 0 0 0 0 1 
+			}else {
+				cout << "Undefined Symbol at line no." << lineno << endl;
+				exit(0);//exit 
+			}
+		} else if (optab.find(vec[i]) != optab.end()){
+			if(optab[vec[i]].format == 3){
+				result.opcode = optab[vec[i]].opcode & 0xFC;
+				result.format = optab[vec[i]].format;
+			}
+			else
+				result.opcode = optab[vec[i]].opcode;
+				result.format = optab[vec[i]].format;
+		}
+		else{
+			cout << "Undefined Symbol at line no." << lineno << endl;
+			exit(0);
+		}
+		/*flags and disp TO DO ADD DIRECT ADDRESSING*/
+		if(optab[vec[i]].format >=2)
+				if(vec[i+1][0] == '#'){
+						result.disp = stoi(vec[1].substr(1));
+						result.flags = result.flags | 0x10;//0 1 0 0 0 0 immediate addressing
+				} else if (symtab.find(vec[i+1]) != symtab.end()){
+						if(pc_relative){
+						result.disp = symtab[vec[i+1]] - r.PC;
+						result.flags = result.flags | 0x32;//1 1 0 0 1 0 pc-relative addressing
+					}else{
+						result.disp = symtab[vec[i+1]] - r.B;
+						result.flags = result.flags | 0x34;//1 1 0 1 0 0 base relative addressing 
+					}
+				}else if(vec[i+1].back() == 'X' && symtab.find(vec[i+1].substr(0,vec[1].length()-2)) != symtab.end()){//LDA ARRAY,X
+					if(pc_relative){
+						result.disp = symtab[vec[i+1].substr(0,vec[1].length()-2)] - r.PC;
+						result.flags = result.flags | 0x3A;//1 1 1 0 1 0 index addressing pc relative 
+					}else{
+						result.disp = symtab[vec[i+1].substr(0,vec[1].length()-2)] - r.B;
+						result.flags = result.flags | 0x34;//1 1 1 1 0 0 index addrssing base relative
+					}
+					result.flags = result.flags | 0x38;	
+				}else if (vec[i+1].front() == '@' && symtab.find(vec[1].substr(1)) != symtab.end()){
+					if(pc_relative){
+						result.disp = symtab[vec[i+1].substr(0,vec[i+1].length()-2)] - r.PC;
+						result.flags = result.flags | 0x22;//1 0 0 0 1 0 pc relative indirect addressing
+					}else{
+						result.disp = symtab[vec[i+1].substr(0,vec[i+1].length()-2)] - r.B;
+						result.flags = result.flags | 0x24;//1 0 0 1 0 0 base relative indirect addressing
+					}
+				}
+				else{
+					cout << "Undefined Symbol at line no." << lineno << endl;
+					exit(0);//exit
+				}
+	return result;
+}
 
 void register_initialize() {
 	registertab['A'] = 0;
-  registertab['X'] = 1;
-  registertab['L'] = 2;
-  registertab['B'] = 3;
-  registertab['S'] = 4;
-  registertab['T'] = 5;
+  	registertab['X'] = 1;
+  	registertab['L'] = 2;
+  	registertab['B'] = 3;
+  	registertab['S'] = 4;
+  	registertab['T'] = 5;
 }
 
 void opcode_initialize() {
-	optab["ADD"]		= 0x18;
-	optab["ADDR"]		= 0x90;
-	optab["DIV"]		= 0x24;
-	optab["DIVR"]		= 0x9C;
-	optab["SUB"]		= 0x1C;
-	optab["SUBR"]		= 0x5C;
-	optab["MUL"]		= 0x20;
-	optab["MULR"]		= 0x98;
+	optab["ADD"].opcode 	= 0x18;
+	optab["ADD"].format 	= 3;
+	optab["ADDR"].opcode 	= 0x90;
+	optab["ADDR"].format 	= 2;
+	optab["DIV"].opcode		= 0x24;
+	optab["DIV"].format 	= 3;
+	optab["DIVR"].opcode	= 0x9C;
+	optab["DIVR"].format 	= 2;
+	optab["SUB"].opcode		= 0x1C;
+	optab["SUB"].format		= 3;
+	optab["SUBR"].opcode	= 0x5C;
+	optab["SUBR"].format	= 2;
+	optab["MUL"].opcode		= 0x20;
+	optab["MUL"].format		= 3;
+	optab["MULR"].opcode	= 0x98;
+	optab["MULR"].format	= 2;
 
-	optab["COMP"]		= 0x28;
-	optab["COMPR"]	= 0xA0;
+	optab["COMP"].opcode	= 0x28;
+	optab["COMP"].format	= 3;
+	optab["COMPR"].opcode	= 0xA0;
+	optab["COMPR"].format	= 2;
 
-	optab["JEQ"]		= 0x30;
-	optab["JGT"]		= 0x34;
-	optab["JLT"]		= 0x38;
-	optab["J"]			= 0x3C;
+	optab["JEQ"].opcode		= 0x30;
+	optab["JEQ"].format 	= 3;
+	optab["JGT"].opcode		= 0x34;
+	optab["JGT"].format 	= 3;
+	optab["JLT"].opcode		= 0x38;
+	optab["JLT"].format 	= 3;
+	optab["J"].opcode		= 0x3C;
+	optab["J"].format 		= 3;
 
-	optab["LDA"]		= 0x00;
-	optab["LDB"]		= 0x68;
-	optab["LDL"]		= 0x08;
-	optab["LDS"]		= 0x6C;
-	optab["LDT"]		= 0x74;
-	optab["LDX"]		= 0x04;
-	optab["LDCH"]		= 0x50;
+	optab["LDA"].opcode		= 0x00;
+	optab["LDA"].format		= 3;
+	optab["LDB"].opcode		= 0x68;
+	optab["LDB"].format		= 3;
+	optab["LDL"].opcode		= 0x08;
+	optab["LDL"].format		= 3;
+	optab["LDS"].opcode		= 0x6C;
+	optab["LDS"].format		= 3;
+	optab["LDT"].opcode		= 0x74;
+	optab["LDT"].format		= 3;
+	optab["LDX"].opcode		= 0x04;
+	optab["LDX"].format		= 3;
+	optab["LDCH"].opcode	= 0x50;
+	optab["LDCH"].format	= 3;
 
-	optab["STA"]		= 0x0C;
-	optab["STB"]		= 0x78;
-	optab["STL"]		= 0x14;
-	optab["STS"]		= 0x7C;
-	optab["STT"]		= 0x84;
-	optab["STX"]		= 0x10;
-	optab["STCH"]		= 0x54;
+	optab["STA"].opcode		= 0x0C;
+	optab["STA"].format		= 3;	
+	optab["STB"].opcode		= 0x78;
+	optab["STB"].format		= 3;
+	optab["STL"].opcode		= 0x14;
+	optab["STL"].format		= 3;
+	optab["STS"].opcode		= 0x7C;
+	optab["STS"].format		= 3;
+	optab["STT"].opcode		= 0x84;
+	optab["STT"].format		= 3;
+	optab["STX"].opcode		= 0x10;
+	optab["STX"].format		= 3;
+	optab["STCH"].opcode	= 0x54;
+	optab["STCH"].format	= 3;
 
-	optab["TIX"]		= 0x2C;
-	optab["TIXR"]		= 0xB8;
+	optab["TIX"].opcode		= 0x2C;
+	optab["TIX"].format 	= 3;
+	optab["TIXR"].opcode	= 0xB8;
+	optab["TIXR"].format 	= 3;
 
-  optab["RSUB"]		= 0x4C;
-  optab["JSUB"]		= 0x48;
+  	optab["RSUB"].opcode	= 0x4C;
+  	optab["RSUB"].format 	= 3;
+  	optab["JSUB"].opcode	= 0x48;
+  	optab["JSUB"].format 	= 3;
 
-  optab["AND"]		= 0x40;
-  optab["SHIFTL"]	= 0xA4;
-  optab["SHIFTR"]	= 0xA8;
-  optab["OR"]			= 0x44;
-  optab["RMO"]		= 0xAC;
+  	optab["AND"].opcode		= 0x40;
+  	optab["AND"].format 	= 3;
+  	optab["SHIFTL"].opcode	= 0xA4;
+  	optab["SHIFTL"].format  = 2;
+  	optab["SHIFTR"].opcode	= 0xA8;
+  	optab["SHIFTR"].format  = 2;
+  	optab["OR"].opcode		= 0x44;
+  	optab["OR"].format		= 2;
+  	optab["RMO"].opcode		= 0xAC;
+  	optab["RMO"].format		= 2;
 }
 
 int main() {
-	int8 memory[MEMORY_SIZE];
 	vector<vector<string>> vec(1000);
 	ifstream fin;
-	int16 locattr = 0;
+	int16 locattr = 0, start_location = 0;
 	istringstream s;
 	string line, word;
-	int lineno = 0, i = 0;
-	bool new_command = false;
+	string program_name;
+	int lineno = 1, i = 0, len;
+	bool new_program = false;
+	int length;
 
 	fin.open("input.txt");
-	opcode_initialize();
 
+	opcode_initialize();
+	/* PASS 1*/
 	while (!fin.eof()) {
 		getline(fin, line);
 		s.str(line);
@@ -93,11 +230,30 @@ int main() {
 			vec[i].push_back(word);
 		}
 		s.clear();
-		if (vec[i].size() < 3) {
+		if (vec[i].size() == 0) {
 			i++;
 			lineno++;
 			continue;
 		}
+		if(vec[i][1] == "START"){
+			if(new_program){
+				cout << "Duplicate START at line no. " << lineno << endl;
+				return 0;
+			}
+			program_name = vec[i][0];
+			locattr = (int16)stoi(vec[i][2]);
+			new_program = true;
+			symtab[vec[i][0]] = locattr;
+			start_location = locattr;
+		}
+		if(vec[i][1] == "END"){
+			if(vec[i][2] == program_name)
+				cout << "Error at line no." << lineno << endl;
+			return 0;
+
+		}
+
+		new_program = true;
 		if (vec[i][0][0] == '+' && optab.find(vec[i][0].substr(1)) != optab.end()) // +jsub extended instruction
 			locattr = locattr + 4;
 		else if (optab.find(vec[i][0]) != optab.end())
@@ -131,26 +287,65 @@ int main() {
 				} else if (vec[i][2][0] == 'X' && vec[i][2][1] == '\'') {
 					if (vec[i][2].length() > 5) {
 						cout << "Invalid value at line no. " << lineno << endl;
-						break;
+						return 0;
 					} else if (vec[i][2][4] == '\'') {
 						++locattr;
 					} else {
-						cout << "Invalid value at line no. " << lineno+1 << endl;
-						break;
+						cout << "Invalid value at line no. " << lineno << endl;
+						return 0;
 					}
-				}
+				} else {// P BYTE abc
+						cout << "Invalid value at line no. " << lineno << endl;
+						return 0;
+					}
+
 			} else {
 				symtab[vec[i][0]] = locattr;
-				if (vec[i][0][1] == '+' && optab.find(vec[i][1].substr(1)) != optab.end()) // +jsub extended instruction
+				if (vec[i][0][1] == '+' && optab.find(vec[i][1].substr(1)) != optab.end()) // loop +jsub extended instruction
 					locattr += 4;
-				else if (optab.find(vec[i][1]) != optab.end())
+				else if (optab.find(vec[i][1]) != optab.end())//loop jsub abc
 					locattr += 3;
-				else
-					cout << "Invalid symbol at line no. " << lineno+1 << endl;
+				else{
+					cout << "Invalid symbol at line no. " << lineno << endl;
+					return 0;
+				}
 			}
 		}
 		i++;
 		lineno++;
 	}
+	fin.close();
+	length = (locattr - start_location);
+	len = i;// len is the total no of line in input.txt
+	/* PASS1 complete */
+
+
+	/* PASS2 START */ 
+	int8 memory[MEMORY_SIZE];
+	ofstream fout;
+	int j=0;
+	instruction objectcode;
+	locattr = start_location;
+	fout.open("output.txt");
+	register_initialize();
+	registers r;
+	r.PC = start_location;
+	fout.setf(ios_base::hex , ios_base::basefield);
+	for (i = 0;i < len;i++){
+		if(vec[i].size() == 0)
+			continue;
+
+		if(vec[i][1] == "START")
+			fout << "H^" << program_name << print(6) << start_location << print(6) << length << endl;
+		else if(vec[i][1] == "END"){
+			fout << "E^" << start_location << print(6) << length << endl;	
+		}
+		else{
+			objectcode = calculate_objectcode(vec[i],i,r);
+			/*memory[locattr] = TO DO*/
+
+		}
+	}
+
 	return 0;
 }
